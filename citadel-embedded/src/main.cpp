7829e404ec23c7c199c00d84aa3c37fe2cd214ac
Jack Schumacher <jackrschumacher@gmail.com>
2026-05-08 21:48:35 -0500

    feat: Updated to use new PWM library for servo movement

--- citadel-embedded/src/main.cpp
+++ citadel-embedded/src/main.cpp
@@ -34,16 +34,32 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define FAN_PWM_MIN 1000 // us  -1.0 duty
 #define FAN_PWM_MAX 2000 // us  1.0 duty
 
+// Fan PWM pin (connected directly to ESP, unlike the others connected through the multiplexer)
 #define FAN_PWM 9
+
+// Other servos PWM pins (connected through the multiplexer)
+#define VALVE_0 1
+#define VALVE_1 2
+#define VALVE_2 3
+
+#define DISTRIBUTOR_0 4
+#define DISTRIBUTOR_1 5
+#define DISTRIBUTOR_2 6
+
+// Typical PWM servo movement
+#define SERVOMOVEMIN 150
+#define SERVOMOVEMAX 600
 
 #define COMMS_UART Serial // To/from USB for debugging
 #define PWM_FREQUENCY 50
 
+// Linear actuator structure to easily allow for extension/retraction of the chemical linear actuators (increase = extend, decrease = retract)
 struct linearActuators
 {
     int chem_increase;
     int chem_decrease;
 };
+// Linear actuator pins
 linearActuators chemicalActuators[3] = {
     {10, 11},
     {9, 8},
@@ -56,28 +72,36 @@ double chemicalDeadzone = 0.05;
 // Constraining the speed values for Chemical linear actuators
 uint16_t chemActuatorsSpeed = constrain(chemActuatorsSpeed, -1.0, 1.0);
 
-// Defined servos (3 for valves, 3 for distributors, 3 for chemicals)
-Servo valve0, valve1, valve2, distributor0, distributor1, distributor2;
+// Create valve and distributor servo arrays with Pin ID
+uint16_t distributorServos[3] = {DISTRIBUTOR_0, DISTRIBUTOR_1, DISTRIBUTOR_2};
+uint16_t valveServos[3] = {VALVE_0, VALVE_1, VALVE_2};
 
 // Track the current distributor positions and the requested distributor positions
 int distributorPos[3] = {0, 0, 0};
 int distributorReq[3] = {0, 0, 0};
-Servo *valves[3] = {&valve0, &valve1, &valve2};
 
 long lastWiggle = 0; // For Distributor servos- count last time moved
 
 unsigned long lastCtrlCmd = 0;
 unsigned long lastMotorStatus = 0;
-
+int servoAngle;
+int pwmPulse;
 // Variables for CAN commands- since all servos in a group should be writing the same
 int valveID;
 int chemicalID;
-
 int distributorID;
 
 bool ledState = false;
 // Control the NEO550 functioning as the fan motor
 Servo fanMotor;
+
+// Since there is no function in this library for natively writing servos (0-180), take raw angle and write to pwm value for that pin
+void writeServo(uint16_t pin, int angle)
+{
+    servoAngle = constrain(angle, 0, 180);
+    pwmPulse = map(angle, 0, 180, SERVOMOVEMIN, SERVOMOVEMAX);
+    pwm.setPWM(pin, 0, angle);
+}
 
 void setup()
 {
@@ -88,26 +112,22 @@ void setup()
     pwm.setPWMFreq(PWM_FREQUENCY);        // External PWM frequency
     Wire.begin(I2C_SDA, I2C_SCL);         // Use the defined SDA and SCL pins
 
-    // Valves are on top of the unit
-    valve0.attach(1);
-    valve1.attach(2);
-    valve2.attach(3);
+    // Loop through all of the servos and close them
+    for (int i = 0; i <= 2; i++)
+    {
+        writeServo(distributorServos[i], 0);
+        writeServo(valveServos[i], 0);
+    }
 
-    // Distributors are on each of the pieces that hang down
-    distributor0.attach(4);
-    distributor1.attach(5);
-    distributor2.attach(6);
+    // Set all Linear actuators to 0 (IDs 0-2)
+    pwm.setPWM(chemicalActuators[0].chem_decrease, 0, 0);
+    pwm.setPWM(chemicalActuators[0].chem_increase, 0, 0);
+    pwm.setPWM(chemicalActuators[1].chem_decrease, 0, 0);
+    pwm.setPWM(chemicalActuators[1].chem_increase, 0, 0);
+    pwm.setPWM(chemicalActuators[2].chem_decrease, 0, 0);
+    pwm.setPWM(chemicalActuators[2].chem_increase, 0, 0);
 
     fanMotor.attach(FAN_PWM, FAN_PWM_MIN, FAN_PWM_MAX);
-
-    // Set all valve, distributor, and chemical servos to their 0 positions
-    valve0.write(0);
-    valve1.write(0);
-    valve2.write(0);
-
-    distributor0.write(0);
-    distributor1.write(0);
-    distributor2.write(0);
 
     fanMotor.writeMicroseconds((FAN_PWM_MIN + FAN_PWM_MAX) / 2);
 
@@ -217,18 +237,18 @@ void loop()
                     distributorReq[i] = canData[i];
                 }
             }
-            // If -1 is passed in, close all valves
-            // Valve movement
+            // Valve movement - valveID must be between 0-2
+            // Open all valves
             if (valveID >= 0 && valveID <= 2)
             {
-                valves[valveID]->write(180);
+                writeServo(valveID, 180);
             }
             // If the valve IDs are not valid or -1 is passed in, loop through and close all valves
             else
             {
-                for (int i = 0; i < 3; i++)
+                for (int valveID = 0; valveID <= 2; valveID++)
                 {
-                    valves[i]->write(0);
+                    writeServo(valveID, 0);
                 }
             }
         }
@@ -265,12 +285,12 @@ void loop()
         lastWiggle = millis();
 
         distributorPos[0] = distributorReq[0] && !distributorPos[0];
-        distributor0.write(distributorPos[0] ? 100 : 0);
+        writeServo(distributorServos[0], distributorPos[0] ? 100 : 0);
 
         distributorPos[1] = distributorReq[1] && !distributorPos[1];
-        distributor1.write(distributorPos[1] ? 100 : 0);
+        writeServo(distributorServos[1], distributorPos[1] ? 100 : 0);
 
         distributorPos[2] = distributorReq[2] && !distributorPos[2];
-        distributor2.write(distributorPos[2] ? 100 : 0);
+        writeServo(distributorServos[2], distributorPos[2] ? 100 : 0);
     }
 }
